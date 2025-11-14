39c6c05ed117a71cf5fadb3301d8197aa49cff2c
sidharth <sidharthas4u@gmail.com>
2025-11-15 03:11:14 +0530

    Create Weather_Observation_Station_11.sql
    hacker rank solution

---
+++ Weather_Observation_Station_11.sql
@@ -0,0 +1,1 @@
+select distinct city from station where (city not like('A%') AND city not like('E%') and city not like('I%') and city not like('O%') and city not like('U%')) or (city not like('%a') AND city not like('%e') and city not like('%i') and city not like('%o') and city not like('%u'));
